b55ada2f071a391727756c119cf0ca2a0f7b392d
Serje <Serje.havandjian@gmail.com>
2022-04-27 11:58:24 -0400

    commit tree ebbc9e55ef2f2d827679bb38bd89d8eae75a3cb0

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution here
+
+// const findMatching = drivers.filter(function(name){
+//     return name
+// })
+
+
+// function findMatching(drivers, driverName){
+//     drivers.filter((driver) => {
+//         driver === driverName
+//     })
+// }
+
+
+function findMatching(drivers, driverName){
+    return drivers.filter(function(name){
+     if(name.toLowerCase() === driverName.toLowerCase()){
+         return name
+     }
+    })
+}
+
+function fuzzyMatch(drivers, driverName){
+    return drivers.filter(function(name){
+        if(name[0] === driverName[0]){
+            return name
+        }
+    })
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+
+function matchName(drivers, driverName){
+    return drivers.filter(function(driver){
+        if (driver.name === driverName){
+            return driver
+        }
+    })
+}
